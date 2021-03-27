97701f7b0db93e2c989dc109ab3798f557a822f9
Michael Capecci <mjcapecci@gmail.com>
2021-03-27 00:46:30 -0500

    installs react and starts setting up OAuth

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import Shopify, { ApiVersion, AuthQuery } from '@shopify/shopify-api';
+import FaunaDbStore from './db/faunadb-store';
 require('dotenv').config();
 
 const app = express();
 
 const { API_KEY, API_SECRET_KEY, SCOPES, SHOP, HOST } = process.env;
+
+const sessionStorage = new FaunaDbStore();
 
 Shopify.Context.initialize({
   API_KEY,
@@ -13,15 +16,19 @@ Shopify.Context.initialize({
   HOST_NAME: HOST,
   IS_EMBEDDED_APP: true,
   API_VERSION: ApiVersion.October20,
+  SESSION_STORAGE: new Shopify.Session.CustomSessionStorage(
+    sessionStorage.storeCallback,
+    sessionStorage.loadCallback,
+    sessionStorage.deleteCallback
+  ),
 });
 
 // define routes
 app.use('/api/auth', require('./routes/auth'));
+app.get('/app', async (req, res) => {
+  res.send('Success');
+});
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log(
-    `Duplicate Product Inspector is now listening on port ${
-      process.env.PORT || 5000
-    }`
-  );
+  console.log(`Server is now listening on port ${process.env.PORT || 5000}`);
 });
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,22 +1,29 @@
-import { AuthQuery, Shopify } from '@shopify/shopify-api';
+import Shopify, { AuthQuery } from '@shopify/shopify-api';
 import { Request, Response } from 'express';
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', async (req: Request, res: Response) => {
-  const { SHOP } = process.env;
-  let authRoute = await Shopify.Auth.beginAuth(
-    req,
-    res,
-    SHOP,
-    '/api/auth/callback',
-    true
-  );
-  return res.redirect(authRoute);
+router.get('/login', async (req: any, res: any) => {
+  // 1. Save store on successful install
+  // 2. Start checking using APP is main app route, and check to see if store exists on there. If it does (a) / does not (b):
+  // a. Serve the app
+  // b. Install the app on the store, and save the store in the DB
+  // 3. Remove FaunaDB functionality and save for a rainy day
+  // 4. Get basic react app with App Bridge up and running
+
+  // let authRoute = await Shopify.Auth.beginAuth(
+  //   req,
+  //   res,
+  //   SHOP,
+  //   '/api/auth/callback',
+  //   false
+  // );
+
+  return res.send('Success (Install)');
+  // return res.redirect(authRoute);
 });
 
-router.get('/callback', async (req: Request, res: Response) => {
-  console.log('TESTER, TESTER, 12345');
+router.get('/callback', async (req: any, res: any) => {
   try {
     await Shopify.Auth.validateAuthCallback(
       req,
@@ -26,7 +33,9 @@ router.get('/callback', async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
   }
-  return res.redirect('/');
+  return res.redirect(
+    `https://${process.env.SHOP}/admin/apps/duplicate-product-dev/app`
+  );
 });
 
 module.exports = router;
